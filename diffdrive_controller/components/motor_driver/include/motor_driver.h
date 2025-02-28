58a224b4be2e0d8ec9fa665f3c038f246bb80767
linzhanguca <lzhang12@uca.edu>
2025-02-28 15:34:51 -0600

    update motor_driver header

--- diffdrive_controller/components/motor_driver/include/motor_driver.h
+++ diffdrive_controller/components/motor_driver/include/motor_driver.h
@@ -1,18 +1,53 @@
 #pragma once
 #include "driver/gpio.h"
 #include "driver/ledc.h"
+#include "esp_err.h"
 
+/**
+ * @brief PH/EN Motor Configuration (Phase: direction; Enable: PWM)
+ */
 typedef struct {
-  gpio_num_t pwm_pin;         // PWM pin (e.g., GPIO_NUM_1)
-  gpio_num_t dir_pin;         // Direction pin (e.g., GPIO_NUM_2)
+  gpio_num_t ph_pin;          // Direction pin (e.g., GPIO_NUM_2)
+  gpio_num_t en_pin;          // PWM pin (e.g., GPIO_NUM_1)
   ledc_channel_t pwm_channel; // LEDC channel (e.g., LEDC_CHANNEL_0)
   ledc_timer_t pwm_timer;     // LEDC timer (e.g., LEDC_TIMER_0)
-} motor_config_t;
+} motor_t;
 
 // Initialize motor driver
-void motor_init(const motor_config_t *config);
+esp_err_t enable_motor(const motor_t *m_cfg);
 
-// Set motor speed and direction
-void motor_set_speed(
-    int speed_percent); // Speed: -100% (reverse) to +100% (forward)
-void motor_set_direction(bool forward); // True = forward, False = reverse
+/**
+ * @brief Set speed for motor
+ *
+ * @param motor: Motor struct
+ * @param speed: Motor speed (dutycycle), -32,768 (reverse) to 32,767 (forward)
+ *
+ * @return
+ *      - ESP_OK: Set motor speed successfully
+ *      - ESP_ERR_INVALID_ARG: Set motor speed failed because of invalid
+ * parameters
+ *      - ESP_FAIL: Set motor speed failed because other error occurred
+ */
+esp_err_t motor_set_speed(motor_t *motor, int16_t speed);
+
+/**
+ * @brief Set motor direction to forward
+ *
+ * @param motor: Motor struct
+ *
+ * @return
+ *      - ESP_OK: Forward motor successfully
+ *      - ESP_FAIL: Forward motor failed because some other error occurred
+ */
+esp_err_t motor_forward(motor_t *motor);
+
+/**
+ * @brief Reverse motor direction
+ *
+ * @param strip: Motor struct
+ *
+ * @return
+ *      - ESP_OK: Reverse motor successfully
+ *      - ESP_FAIL: Reverse motor failed because some other error occurred
+ */
+esp_err_t motor_reverse(motor_t *motor);
