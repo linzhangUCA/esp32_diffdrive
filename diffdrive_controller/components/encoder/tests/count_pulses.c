44b2fb0ae2e47abf6c643a16adc7fe0d76a7df93
linzhanguca <lzhang12@uca.edu>
2025-03-06 15:40:48 -0600

    update test code

--- diffdrive_controller/components/encoder/tests/count_pulses.c
+++ diffdrive_controller/components/encoder/tests/count_pulses.c
@@ -6,10 +6,10 @@ static const char *TAG = "Encoder";
 
 void app_main(void) {
   // Configure encoder
-  encoder_config_t enc_cfg = {
+  encoder_config_t encoder_cfg = {
       .enc_a_pin = GPIO_NUM_13,
       .enc_b_pin = GPIO_NUM_14,
-      // .pcnt_unit = NULL,
+      .pcnt_unit = NULL,
       .counts_per_rev = 4096,
   };
 
@@ -18,11 +18,11 @@ void app_main(void) {
   int pulse_count = 0;
 
   // Initialize encoder
-  encoder = encoder_init(&enc_cfg);
+  enable_encoder(&encoder_cfg);
 
   // Read encoder counts
   for (i = 0; i < 200; i++) {
-    pcnt_unit_get_count(encoder, &pulse_count);
+    pcnt_unit_get_count(&encoder_cfg, &pulse_count);
     ESP_LOGI(TAG, "Pulse count: %d", pulse_count);
     vTaskDelay(100 / portTICK_PERIOD_MS);
   }
